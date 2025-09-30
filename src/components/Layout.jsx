7829c961edf543f8851158fa60fb01064c50ccb2
nihinivi <nihinivi100@gmail.com>
2025-09-30 18:00:32 +0530

    remove the footer and add wave animation registration page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom'; // Outlet renders the current route's component
+import { Outlet, useLocation } from 'react-router-dom'; 
 import Navbar from './Navbar';
 import FooterSection from './FooterSection';
-
+import Waves from './Waves';
 const Layout = () => {
+  const location = useLocation();
+  const blacklist = ['/eventdetails'];
+  const hideFooter = blacklist.includes(location.pathname); 
   return (
-    <>
-      <Navbar />
+    <> 
+
+     {hideFooter && 
+<Waves
+  lineColor="#121212"
+  backgroundColor="rgba(255, 255, 255, 0.2)"
+  waveSpeedX={0.02}
+  waveSpeedY={0.01}
+  waveAmpX={40}
+  waveAmpY={20}
+  friction={0.9}
+  tension={0.01}
+  maxCursorMove={120}
+  xGap={12}
+  yGap={36}
+/>}
+     {!hideFooter && <Navbar />}
       <main>
-        {/* The active page (e.g., HomePage or EventDetails) will be rendered here */}
         <Outlet />
       </main>
-      <FooterSection />
+      {!hideFooter && <FooterSection />}
     </>
   );
 };
